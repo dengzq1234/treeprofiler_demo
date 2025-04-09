4da3a04cad30972b4e99fdc578ea3d896f04edb3
dengzq1234 <dengziqi1234@gmail.com>
2025-04-09 12:49:10 +0200

    update run gtdb profiling

--- gtdb_archaea_profiling/run_treeprofiler.sh
+++ gtdb_archaea_profiling/run_treeprofiler.sh
@@ -1,7 +1,7 @@
 # run annotation
 treeprofiler annotate \
 -t ./ar53_r207.nw \
--d cdh_mcr_mtr_kegg_profiling_matrix.csv \
+--metadata cdh_mcr_mtr_kegg_profiling_matrix.csv \
 --counter-stat raw \
 --ls-columns cdhA cdhB cdhC mcrA mcrB mcrC mcrD mcrG mtrA mtrB mtrC mtrD mtrE mtrF mtrG mtrH \
 --taxon-column name \
@@ -17,7 +17,8 @@ treeprofiler plot \
 --tree ar53_r207_annotated.ete \
 --taxoncollapse-layout \
 --ls-layout cdhA cdhB cdhC mcrA mcrB mcrC mcrD mcrG mtrA mtrB mtrC mtrD mtrE mtrF mtrG mtrH \
---binary-layout cdhA cdhB cdhC mcrA mcrB mcrC mcrD mcrG mtrA mtrB mtrC mtrD mtrE mtrF mtrG mtrH \
+--binary-unicolor-layout cdhA cdhB cdhC mcrA mcrB mcrC mcrD mcrG mtrA mtrB mtrC mtrD mtrE mtrF mtrG mtrH \
 --column-width 30 \
 --padding-x 2 \
---taxonclade-layout
+--taxonclade-layout \
+--port 5005
